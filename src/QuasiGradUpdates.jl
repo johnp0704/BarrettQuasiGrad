b195b8cd8b01b944ac7085eacfbba2b20d390dca
johnp0704 <jlpoirie@uvm.edu>
2024-07-10 10:21:51 -0400

    Matrices Testing

--- src/QuasiGradUpdates.jl
+++ src/QuasiGradUpdates.jl
@@ -1,31 +1,4 @@
 module QuasiGradUpdates
 using CUDA
 
-
-A_out = Vector{CuArray{Float32, 2}}()
-x_out = Vector{CuArray{Float32, 2}}()
-y_out = Vector{CuArray{Float32, 2}}()
-
-for i in 1:1000
-    A_i = randn(Float32,10,10)
-    A_gpu = cu(A_i)
-    push!(A_out, A_gpu)
-    
-    x_i = randn(Float32,10,1)
-    x_gpu = cu(x_i)
-    push!(x_out, x_gpu)
-
-    y_gpu = A_gpu * x_gpu
-    push!(y_out, y_gpu)
-
-    println(A_out[i])
-    println(x_out[i])
-    println(y_out[i])
 end
-
-end
-
-println()
-#this has been added to test pushing
-
-
