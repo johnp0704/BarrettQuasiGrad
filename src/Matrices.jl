154f38f7776e13c9bc95cd5c6fa111712370a500
johnp0704 <jlpoirie@uvm.edu>
2024-07-14 12:01:19 -0400

    btime Testing

--- src/Matrices.jl
+++ src/Matrices.jl
@@ -1,27 +1,71 @@
 using CUDA
-A_out = Vector{CuArray{Float32, 2}}()
-x_out = Vector{CuArray{Float32, 2}}()
-y_out = Vector{CuArray{Float32, 2}}()
+using BenchmarkTools
+#A_out = Vector{CuArray{Float32, 2}}()
+#x_out = Vector{CuArray{Float32, 2}}()
+#y_out = Vector{CuArray{Float32, 2}}()
 
-for i in 1:1000
-    A_i = randn(Float32,10,10)
-    A_gpu = cu(A_i)
-    push!(A_out, A_gpu)
+#for i in 1:1000 #don't loop, all at once!
+ #   A_i = randn(Float32,10,10)
+  #  A_gpu = cu(A_i)
+   # push!(A_out, A_gpu)
     
-    x_i = randn(Float32,10,1)
-    x_gpu = cu(x_i)
-    push!(x_out, x_gpu)
+   # x_i = randn(Float32,10,1)
+    #x_gpu = cu(x_i)
+    #push!(x_out, x_gpu)
 
-    y_gpu = A_gpu * x_gpu
-    push!(y_out, y_gpu)
+    #y_gpu = A_gpu * x_gpu
+    #push!(y_out, y_gpu)
 
-    println(A_out[i])
-    println(x_out[i])
-    println(y_out[i])
+    #println(A_out[i])
+    #println(x_out[i])
+    #println(y_out[i])
+#end
+#Above is old solution
+
+
+
+#x = randn(1000,10)
+#function x_squared(x::Matrix{Float64}, y) 
+    #y .= x .^2   #y . says take every element of array and overwrite on y, makes it not allocate memory
+#end
+
+#y = similar(x)
+#@time x_squared(x,y)
+
+    #@btime #runs like 1000 iterations of the code
+#@btime x_squared(x,y) #evaluates 1000 times, then give statistics
+
+#Above is all for CPU only, example code
+
+
+
+
+#%% GPU
+
+A = randn(10,10)
+A_gpu = cu(A)
+
+x = randn(10,1)
+x_gpu = cu(x)
+
+function y_calc_gpu(A_gpu::CuArray{Float32,2}, x_gpu::CuArray{Float32,2})
+    return A_gpu * x_gpu
 end
+y_gpu = similar(x_gpu)
+
+@time y_gpu .= y_calc_gpu(A_gpu, x_gpu) #gives ~0.010617 seconds, 711 allocations
+@btime y_gpu .= y_calc_gpu(A_gpu, x_gpu) #gives ~30.400 us, 104 allocations
 
 
-println()
-#this has been added to test pushing
+#%% CPU
 
+B = randn(10,10)
 
+w = randn(10,1)
+
+function z_calc_cpu(B, w)
+    return B * w
+end
+z = similar(x)
+@time z .= z_calc_cpu(B, w) #0.004153 seconds, 391 allocations
+@btime z .= z_calc_cpu(B, w) #192.235 ns, 2 allocations
